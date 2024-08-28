e00a38530033f9c9ff6a7a8b0c0a38b43f0a3a67
jie.chen@enjoyelec.net <jie.chen@enjoyelec.net>
2024-08-28 11:35:25 +0800

    0.0.3

--- upload.sh
+++ upload.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-VersionString=`grep -E 's.version.*=' CTNetworking.podspec`
-VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
-NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' CTNetworking.podspec | cut -d : -f1`
+versionString=$(grep -E 'spec.version.*=' TXQuestionManager.podspec)
+versionNumber=$(tr -cd [^0-9.] <<<"$versionString")
+version=$(echo "$versionNumber" | sed 's/^\.//')
+
+echo 'versionString'=$versionString
+echo 'versionNumber'=$versionNumber
+echo 'version'=$version
 
 git add .
-git commit -am modification
-git pull origin master --tags
+git commit -m $version
+git push origin main
+git tag $version
+git push origin main --tags
 
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" CTNetworking.podspec
-
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
-say "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
-
-git commit -am ${NewVersionNumber}
-git tag ${NewVersionNumber}
-git push origin master --tags
-pod trunk push ./CTNetworking.podspec --verbose --use-libraries --allow-warnings
+# pod spec lint ./TXQuestionManager.podspec --verbose --allow-warnings
+pod trunk push ./TXQuestionManager.podspec --verbose --use-libraries --allow-warnings
